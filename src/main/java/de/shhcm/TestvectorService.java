26e2db52cd494a13a9d8f8f266f83771d07b9069
shhcm <outlaw@gmx-topmail.de>
2014-03-18 22:02:09 +0100

    Added spring.xml configuration.
    The path to  the spring.xml is passed as a jndi Resource, so that development/production servers may be passed different beans (e.g. for database access and other configuration parameters)

--- src/main/java/de/shhcm/TestvectorService.java
+++ src/main/java/de/shhcm/TestvectorService.java
@@ -12,8 +12,20 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+import de.shhcm.beans.TestBean;
+
 /**
- * Root resource (exposed at "myresource" path)
+ * Testing:
+ * mvn jetty:run
+ * curl -v --request POST  -H 'Content-type: application/json' localhost:8081/rest/myresource
+ * 
+ * -or-
+ * 
+ * Configuration for run-jetty-run eclipse plugin for jetty 9.0 (run configurations):
+ * Show Advanced Options -> JNDI Support, Additional jetty.xml
+ * 
  * TODO: WebService that returns test vectors.
  *  
  *  - Allow to post a symmetric key (BASE64), block size, plain text (not longer than the block size), 
@@ -33,13 +45,18 @@ public class TestvectorService {
         System.out.println("Initialized!");
         
         // Lookup JNDI resources. (path to spring.xml and log4j.properties)
-        
         InitialContext initialContext = new InitialContext();
         Context envContext = (Context) initialContext.lookup("java:comp/env");
         System.out.println("Got Context...");
-        String pathToLog4jProperties = (String) envContext.lookup("log4j_config_file_path");
+        String pathToLog4jProperties = (String) envContext.lookup("log4j_config_file_path"); // TODO
         System.out.println(pathToLog4jProperties);
+        String pathToSpringXml = (String) envContext.lookup("spring_xml_file_path");
         
+        // Get instance of FileSystemXmlApplicationContext, need to pass a URI here: file:///path
+        FileSystemXmlApplicationContext fileSystemXmlApplicationContext = new FileSystemXmlApplicationContext(pathToSpringXml);
+        TestBean testBean = (TestBean) fileSystemXmlApplicationContext.getBean("TestBean");
+        System.out.println(testBean.getFoo());
+        fileSystemXmlApplicationContext.close();
     }
     
     @GET
---
+++ src/main/java/de/shhcm/beans/TestBean.java
@@ -0,0 +1,13 @@
+package de.shhcm.beans;
+
+public class TestBean {
+    private String foo;
+
+    public String getFoo() {
+        return foo;
+    }
+
+    public void setFoo(String foo) {
+        this.foo = foo;
+    }
+}
